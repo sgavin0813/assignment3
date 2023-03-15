8a0cbb7927105fdd93695b6b2943bf5b507264ec
sgavin0813 <s.gavin0813@gmail.com>
2023-03-15 01:25:51 -0400

    start

---
+++ app/src/main/java/com/kroger/classdemoapp/Character.kt
@@ -0,0 +1,12 @@
+package com.kroger.classdemoapp
+
+data class Character(
+    val name: String,
+    val age: Int,
+    val image: String,
+    val gender: String,
+    val universe: String,
+    val id: Int,
+    val relation: List<Int>
+)
+
